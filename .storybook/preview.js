681de08ee8c5759c2e13374d2889d846ad4a8860
Karol Zawadzki <karol.zawadzki@knowde.com>
2022-06-30 08:11:47 +0200

    add queryClient provider to storybook config

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,16 +2,32 @@ import * as React from 'react';
 import { GlobalStyles } from '../core/styles';
 import { initialize as initializeMSW, mswDecorator } from 'msw-storybook-addon';
 import { ThemeWrapper } from './theme-wrapper';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
-const themeDecorator = story => <ThemeWrapper>
-  <GlobalStyles />
-  {story()}
-</ThemeWrapper>
+const themeDecorator = (story) => (
+  <ThemeWrapper>
+    <GlobalStyles />
+    {story()}
+  </ThemeWrapper>
+);
+
+const mockedQueryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+    },
+  },
+});
+
+const queryClientDecorator = (story) => (
+  <QueryClientProvider client={mockedQueryClient}>
+    {story()}
+  </QueryClientProvider>
+);
 
 initializeMSW();
 
-export const decorators = [mswDecorator, themeDecorator];
-
+export const decorators = [mswDecorator, queryClientDecorator, themeDecorator];
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
@@ -21,4 +37,4 @@ export const parameters = {
       date: /Date$/,
     },
   },
-}
+};
